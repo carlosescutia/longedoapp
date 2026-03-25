bb5e90c751ad8cb2fb46602c2e596688b0505814
Carlos Escutia <carlosescutia@gmail.com>
2026-03-25 13:58:53 -0600

    Se agregan templates de header y footer.

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,7 @@
+        <div class="ui inverted vertical footer segment">
+            <div class="ui container">
+                <p><em>&copy; 2026</em></p>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="<?=base_url()?>favicon.png"/>
+
+        <title>phpapp4</title>
+
+        <link href="<?=base_url()?>assets/css/phpapp4.css" rel="stylesheet"/>
+
+        <!-- jquery -->
+        <script src="<?=base_url()?>assets/js/jquery.min.js"></script>
+
+        <!-- semantic-ui -->
+        <link href="<?=base_url()?>assets/css/semantic.min.css" rel="stylesheet"/>
+        <script src="<?=base_url()?>assets/js/semantic.min.js"></script>
+
+        <!-- datatables -->
+        <link href="<?=base_url()?>assets/css/datatables.min.css" rel="stylesheet"/>
+        <script src="<?=base_url()?>assets/js/datatables.min.js"></script>
+
+        <!-- chart.js -->
+        <script src="<?=base_url()?>assets/js/chart.js"></script>
+        <script src="<?=base_url()?>assets/js/chartjs-plugin-datalabels"></script>
+
+    </head>
+    <body class="site">
